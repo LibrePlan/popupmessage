e5fb5097beda280c3286ee383fc1eda22b47ad74
Farruco Sanjurjo <fsanjurjo@igalia.com>
2011-03-21 10:22:37 +0100

    Added an extra css class to create shadows

--- src/main/resources/web/js/components/popupmessage/mold/popup-message.js
+++ src/main/resources/web/js/components/popupmessage/mold/popup-message.js
@@ -1,5 +1,5 @@
 function(out){
-		out.push('<div class="popupmessage roundedcorner" ', this.domAttrs_(), '>');
+		out.push('<div class="popupmessage roundedcorner shadow" ', this.domAttrs_(), '>');
 		
 		out.push('<div class="title roundedcorner">', this.getTitle(), '</div>');
 		out.push('<div class="content">', this.getContent(), '</div>');
