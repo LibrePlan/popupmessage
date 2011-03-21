b79a462991e4fad64939eb64ba682d4bb5023718
Farruco Sanjurjo <fsanjurjo@igalia.com>
2011-03-21 10:06:14 +0100

    Added an after-function for title and content setters

--- src/main/resources/web/js/components/popupmessage/PopupMessage.js
+++ src/main/resources/web/js/components/popupmessage/PopupMessage.js
@@ -3,14 +3,13 @@ zk.$package("components.popupmessage");
 components.popupmessage.PopupMessage = zk.$extends(zk.Widget,{
 	$define : {
 		effectName : null,
-		title : null,
-		content : null,
+		title : [null, function(){this._update("title", this.getTitle());}],
+		content : [null, function(){this._update("content", this.getContent());}],
 		autoHide : null
 	},
 	bind_ : function(){
 		this.$supers('bind_', arguments);
 		this.domListen_(jq('.popupmessage .close'), "onClick", '_hideMessage');
-		this._showMessage();
 	},
 	unbind_ : function(){
 		this.domUnlisten_(jq('.popupmessage .close'), "onClick", '_hideMessage');
@@ -20,12 +19,15 @@ components.popupmessage.PopupMessage = zk.$extends(zk.Widget,{
 		jq( "div .popupmessage" ).show(this.getEffectName(),
 										{}, //Options
 										1000, //Effect duration
-										this.isAutoHide() ? this._autoHideCallback : null);
+										this.isAutoHide() ? this.proxy(this._autoHideCallback) : null);
 	},
 	_hideMessage : function() {
 		jq( "div .popupmessage:visible" ).fadeOut(1000);
 	},
 	_autoHideCallback : function(){
 		setTimeout(this._hideMessage, 3000);
+	},
+	_update : function (cssClass, value ){
+		jq(".popupmessage " + "." + cssClass).empty().append(value);
 	}
 })
