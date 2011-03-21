2d10caa2616b8bf3b547b40306c004906ef94a4c
Farruco Sanjurjo <fsanjurjo@igalia.com>
2011-03-21 10:05:46 +0100

    Deleted css class

--- src/main/resources/web/js/components/popupmessage/mold/popup-message.js
+++ src/main/resources/web/js/components/popupmessage/mold/popup-message.js
@@ -2,7 +2,7 @@ function(out){
 		out.push('<div class="popupmessage roundedcorner" ', this.domAttrs_(), '>');
 		
 		out.push('<div class="title roundedcorner">', this.getTitle(), '</div>');
-		out.push('<div class="content roundedcorner">', this.getContent(), '</div>');
+		out.push('<div class="content">', this.getContent(), '</div>');
 		out.push('<div class="close"><a href="#">close</a></div>');
 		
 		out.push('</div>');
